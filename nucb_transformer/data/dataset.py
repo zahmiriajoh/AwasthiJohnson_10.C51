7bf066f2776cbc115d933fead2c33dea604d83f1
Zahmiria Johnson <zahmiria@gmail.com>
2026-04-23 20:24:48 -0400

    update to DataLoaders fxn

--- nucb_transformer/data/dataset.py
+++ nucb_transformer/data/dataset.py
@@ -90,3 +90,5 @@ def make_dataloaders(
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_loader, val_loader, test_loader
